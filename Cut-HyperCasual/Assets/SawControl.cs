cce7d6da509726c86e4f59bae5737539cf4b191e
MUHAMMED RAŞİD KÜÇÜKYILMAZ <95505121+olamazm@users.noreply.github.com>
2022-06-26 17:06:05 +0300

    Güncel

--- Cut-HyperCasual/Assets/SawControl.cs
+++ Cut-HyperCasual/Assets/SawControl.cs
@@ -10,12 +10,11 @@ public class SawControl : MonoBehaviour
     public float Max_X;
     public float Min_X;
     public DynamicJoystick joystick;
+    public GameObject CutFX;
 
     void FixedUpdate()
     {
-        float InputX = joystick.Horizontal;
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, Min_X, Max_X), transform.position.y, transform.position.z);
-        transform.Translate(new Vector3(InputX * LRSpeed * Time.deltaTime, 0, Speed * Time.deltaTime), Space.World);
+        MovementSaw();
     }
 
     private void OnTriggerEnter(Collider col)
@@ -24,6 +23,7 @@ public class SawControl : MonoBehaviour
         {
             LRSpeed = 0;
             Speed = 2;
+            CutFX.SetActive(true);
         }
     }
 
@@ -34,6 +34,7 @@ public class SawControl : MonoBehaviour
             Cutter(col);
             LRSpeed = 10;
             Speed = 10;
+            CutFX.SetActive(false);
         }
     }
 
@@ -57,5 +58,27 @@ public class SawControl : MonoBehaviour
         Destroy(CutLeft, 5);
         Destroy(CutRight, 5);
         Destroy(col.gameObject);
+    }
+
+    private void MovementSaw()
+    {
+        // Movement
+        float InputX = joystick.Horizontal;
+        transform.Translate(new Vector3(InputX * LRSpeed * Time.deltaTime, 0, Speed * Time.deltaTime), Space.World);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, Min_X, Max_X), transform.position.y, transform.position.z);
+
+        // Rotation
+        if (InputX > 0.1f && transform.position.x < Max_X)
+        {
+            transform.Rotate(new Vector3(0, InputX * LRSpeed * Time.deltaTime * 67, 0), Space.World);
+        }
+        if (InputX < -0.1f && transform.position.x > Min_X)
+        {
+            transform.Rotate(new Vector3(0, InputX * LRSpeed * Time.deltaTime * 67, 0), Space.World);
+        }
+        if (InputX <= -0.1f || InputX >= 0.1f || InputX == 0)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, 0.4f);
+        }
     }
 }
